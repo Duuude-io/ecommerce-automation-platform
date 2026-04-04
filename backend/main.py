fc3620f168136694d31b11c36536f271e3b80b0b
Dude_io <adeyemioladokun816@gmail.com>
2026-04-04 06:45:53 +0100

    turned FastAPI to web server

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,27 @@
-# main.py
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from typing import List, Optional
 from pydantic import BaseModel
 from data.products import products
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI(title="Amazon Project Backend")
+
+# Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow frontend access
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# ⭐ SERVE IMAGES FOLDER
+app.mount("/images", StaticFiles(directory="images"), name="images")
+app.mount("/scripts", StaticFiles(directory="scripts"), name="scripts")
+app.mount("/styles", StaticFiles(directory="styles"), name="styles")
+
 # Pydantic models
 
 
@@ -30,12 +39,11 @@ class ProductModel(BaseModel):
     type: Optional[str] = None
     sizeChartLink: Optional[str] = None
 
+
 # Root route
-
-
 @app.get("/")
-def root():
-    return {"message": "Welcome to Amazon Project Backend!"}
+def serve_home():
+    return FileResponse("index.html")
 
 # Products route
 
