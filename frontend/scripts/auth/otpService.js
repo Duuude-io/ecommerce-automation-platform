ca56a0dfe012442532141e66bae46113f99dc531
Dude_io <adeyemioladokun816@gmail.com>
2026-06-17 15:40:25 +0100

    otpServices.js

--- frontend/scripts/auth/otpService.js
+++ frontend/scripts/auth/otpService.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../config";
+import { API_BASE_URL } from "../config.js";
 
 export async function verifyOtp({ userId, otp, purpose }) {
   console.log("Sending to Backend:", { userId, otp, purpose });
