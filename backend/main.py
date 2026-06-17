01d138921701d34a532526e608e03467ff0811a6
Dude_io <adeyemioladokun816@gmail.com>
2026-06-17 05:33:21 +0100

    refactor requirements

--- backend/main.py
+++ backend/main.py
@@ -59,11 +59,8 @@ PRODUCTS_FILE = DATA_DIR / "products.json"
 # Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
